43ad231d8e6a0f8060fbe468dc719a3690436568
Malditoinsanoyvola <seba_123-bkn@hotmail.com>
2014-09-08 20:56:53 -0300

    funcion obtener pelicula lista

---
+++ funciones.py
@@ -0,0 +1,17 @@
+def obtener_pelicula(codigo):
+    peliculas = open('movies.txt')
+    letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    peli = ''
+    for linea in peliculas:
+        inf = linea.strip().split()
+        if inf[0] == codigo:
+            for d in inf:
+                c = 0
+                if d[0] != '(' and d != codigo:
+                    peli += ' ' + d 
+                if d[0] == '(':
+                    if d[1] in letras:
+                        peli += ' ' + d
+    peli = peli.split()
+    peli = ' '.join(peli)
+    return peli
