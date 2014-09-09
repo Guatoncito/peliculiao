5700d0347acaa4e67374d477240cceb0781e5eb9
Guatoncito <benja_as@hotmail.com>
2014-09-08 23:42:39 -0300

    señor jesú funciona la wea

--- Main.py
+++ Main.py
@@ -5,18 +5,23 @@ principal=zeros((943,1682))
 usuario_a_recomendar=10
 usuario_recom=0
 mayor_correl=0
-baenados=list()
+baneados=list()
+recomendadas=list()
 for linea in usuarios:
 	linea=linea.strip().split(' ')
 	principal[(int(linea[0])-1),(int(linea[1])-1)]=int(linea[2])
+	
 while True:
-    for p in range(1,944):
-	    correl=corr(usuario_a_recomendar,p,principal)
-	    if correl>mayor_correl and p not in baneados:
-		    usuario_recom=p
-	if len(peliculas_no_comun(usuario_a_recomendar,usuario_recom,principal))==0:
-		baneados.append(usuario_recom)
-		mayor_correl=0
-		continue
-	break
-print peliculas_no_comun(usuario_a_recomendar,usuario_recom,principal)
+        for p in range(1,944):
+                correl=corr(usuario_a_recomendar,p,principal)
+                if correl>mayor_correl and (p not in baneados):
+                        usuario_recom=p
+        peliculas_recomendadas=peliculas_no_comun(usuario_a_recomendar,usuario_recom,principal)
+        if len(peliculas_recomendadas)==0:
+                baneados.append(usuario_recom)
+                mayor_correl=0
+                continue
+        break
+for pelicu in peliculas_recomendadas:
+	recomendadas.append(obtener_pelicula(pelicu))
+print recomendadas
