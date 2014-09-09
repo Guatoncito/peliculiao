a46159f51fbf6811896c00e71cce4fec091bc9c4
Guatoncito <benja_as@hotmail.com>
2014-09-08 22:07:36 -0300

    peliculas en comun

--- funciones.py
+++ funciones.py
@@ -1,5 +1,3 @@
-
-=======
 def obtener_pelicula(codigo):
     peliculas = open('movies.txt')
     letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
@@ -17,4 +15,12 @@ def obtener_pelicula(codigo):
     peli = peli.split()
     peli = ' '.join(peli)
     return peli
->>>>>>> origin/master
+
+def peliculas_en_comun(usuario1,usuario2,matriz):
+    comun=list()
+    for i in range(1682):
+        if matriz[usuario1-1,i]!=0 and matriz[usuario2-1,i]!=0:
+            comun.append(i)
+    return comun
+
+
